9f512de02670a7e3a696cbce8838b81b2893f798
tinh.nguyen <tinhnguyenvan91@gmail.com>
2021-06-05 16:44:18 +0700

    update key menu

--- config/constant.php
+++ config/constant.php
@@ -8,19 +8,19 @@ return [
             'url' => '',
             'icon' => 'fa fa-cube',
             'child' => [
-                [
+                'nav.menu_left.product_categories' => [
                     'title' => 'nav.menu_left.product_categories',
                     'plugin' => 'woocommerce',
                     'url' => 'woocommerce/product_categories',
                     'icon' => 'fa fa-sitemap',
                 ],
-                [
+                'nav.menu_left.product_list' => [
                     'title' => 'nav.menu_left.product_list',
                     'plugin' => 'woocommerce',
                     'url' => 'woocommerce/products',
                     'icon' => 'fa fa-list',
                 ],
-                [
+                'nav.menu_left.add' => [
                     'title' => 'nav.menu_left.add',
                     'plugin' => 'woocommerce',
                     'url' => 'woocommerce/products/create',
@@ -34,13 +34,13 @@ return [
             'url' => '',
             'icon' => 'fa fa-shopping-bag',
             'child' => [
-                [
+                'nav.menu_left.orders_list' => [
                     'title' => 'nav.menu_left.orders_list',
                     'plugin' => 'woocommerce',
                     'url' => 'woocommerce/orders',
                     'icon' => 'fa fa-shopping-cart',
                 ],
-                [
+                'nav.menu_left.orders_report' => [
                     'title' => 'nav.menu_left.orders_report',
                     'plugin' => 'woocommerce',
                     'url' => 'woocommerce/orders/report',
